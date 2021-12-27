d526554e5a5c3a8be205b992cb8698f990f739a0
jes <sample@gmail.com>
2021-12-27 13:36:10 +0800

    timeout check

--- proxy.go
+++ proxy.go
@@ -36,7 +36,7 @@ var ipReg = regexp.MustCompile(ipPattern)
 var errIp = errors.New("bad ip format")
 var errAddr = errors.New("bad addr format")
 var errPort = errors.New("bad port format")
-var errTimeout = errors.New("bad timeout format")
+var errTimeout = errors.New("timeout must be greater than 0")
 
 func portCheck(s string) error {
 	a, err := strconv.Atoi(s)
@@ -84,7 +84,7 @@ func New(name, local, remote string, timeout time.Duration, failFast bool, logge
 		return nil, err
 	}
 
-	if timeout < 0 {
+	if timeout <= 0 {
 		return nil, errTimeout
 	}
 
